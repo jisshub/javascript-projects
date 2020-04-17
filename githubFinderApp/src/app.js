219a20202790055a74aeb50a2ecd6e1b9ad7b2a9
jisshub <jissmon476@gmail.com>
2020-04-18 01:33:41 +0530

    first commit

---
+++ githubFinderApp/src/app.js
@@ -0,0 +1,7 @@
+export const getInpText = document.getElementById("userName")
+.addEventListener("keyup",(e)=>{
+    const userValue = e.target.value;
+    if(userValue){
+        
+    }
+}) 
---
+++ githubFinderApp/src/github.js
@@ -0,0 +1,11 @@
+export class Github{
+    constructor(){
+        this.client_id ="6d152b3b20379347b792";
+        this.client_secret = "0f6c7eaeef6c1433e273831f0e229e65eea5630b";
+
+    }
+    // to fetch user profile and user repo.
+    async geUser(){
+
+    }
+}

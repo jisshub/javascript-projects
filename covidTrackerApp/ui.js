807a91555f3462e1edb3a410818673a9fae407c9
jisshub <jissmon476@gmail.com>
2020-04-19 15:39:04 +0530

    timestamp issue

--- covidTrackerApp/ui.js
+++ covidTrackerApp/ui.js
@@ -9,12 +9,23 @@ class VirusUI{
         // provide html template to element
         
         userData.forEach(e => {
+            console.log(e.Date);
+            let q = Math.round(new Date(e.Date).getTime()/1000);
         this.currentStatus.innerHTML = 
         `
         <div class="row">
-            <div class="col col-md-6">
-                <h2>${e.Country}</h2><br><br>
-                <div class="info">
+            <div class="col col-md-12">
+                <div class="card card-body text-center">
+                    <h1>${e.Country}</h1><br>
+                    <h2>Coronavirus Cases</h2>
+                    <span class="d-block text-center text-secondary h3">${e.Confirmed}</span><br>
+                    <h2>Total Deaths</h2>
+                    <span class="d-block text-center h3 text-danger">${e.Deaths}</span><br>
+                    <h2>Total Recoveries</h2>
+                    <span class="d-block text-center h3 text-success">${e.Recovered}</span><br>
+                    <h2>Active Cases</h2>
+                    <span class="d-block text-center text-warning h3">${e.Active}</span><br>
+                    <span class="text-center font-weight-normal">Last updated on ${new Date(q)}</span>
                 </div>
             </div>
         </div>
@@ -25,3 +36,8 @@ class VirusUI{
       
     }
 }
+
+
+
+// const q = Math.round(new Date("2020-04-19T00:00:00Z").getTime()/1000);
+// console.log(new Date(q))

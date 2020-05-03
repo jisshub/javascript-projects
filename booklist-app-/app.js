2810a6cfde63f3e9a9ec45d261dc6acc0da9d2ef
jisshub <jissmon476@gmail.com>
2020-05-03 09:19:33 +0530

    cleared inouts fiedlds - books added to table

--- booklist-app-/app.js
+++ booklist-app-/app.js
@@ -15,45 +15,81 @@ class UI{
     }
     static displayBooks(){
         // define a dummy data
-        const storedData = [
-            {
-                title: 'Book One',
-                author: 'John Doe',
-                isbn: '442343'
-            },
-            {
-                title: 'Book Two',
-                author: 'John james',
-                isbn: '442333'
-            },
-        ]
-        const books = storedData;
-        // iterate thru all objects in array
-        books.forEach((eachBook) =>{
-            // call addBooks() and pass book object
-            UI.addBooks(eachBook);
-        })
+        // const storedData = [
+        //     {
+        //         title: 'Book One',
+        //         author: 'John Doe',
+        //         isbn: '442343'
+        //     },
+        //     {
+        //         title: 'Book Two',
+        //         author: 'John james',
+        //         isbn: '442333'
+        //     },
+        // ]
+        // const books = storedData;
+        // // iterate thru all objects in array
+        // books.forEach((eachBook) =>{
+        //     // call addBooks() and pass book object
+        //     UI.addBooks(eachBook);
+        // })
     }
     // get book object
     static addBooks(getBook){
         // add each book to table as row - grab table body id.
         // append a tr child to it 
         let trTag = document.getElementById("bookList").appendChild(document.createElement("tr"));
-        // add innerhtml to tr tag
+        // add innerhtml to tr tag - access each book property
         trTag.innerHTML = `
-            <td>${getBook.title}</td>
-            <td>${getBook.author}</td>
-            <td>${getBook.isbn}</td>
-            <td><a href="#" class="btn btn-danger btn-sm" id="deleteThisRow"></a></td>
+            <td>${getBook.bookTitle}</td>
+            <td>${getBook.bookAuthor}</td>
+            <td>${getBook.bookISBN}</td>
+            <td><a href="#" class="btn btn-danger btn-sm" id="deleteThisRow">X</a></td>
         `
     }
-    // while looping child element tr is appended at bottom
+
+    // clear inouts fields
+    static clearFields(){
+        // grab ech inout value
+        document.querySelector("#title").value = "";
+        document.querySelector("#author").value = "";
+        document.querySelector("#isbn").value = "";
+
+    }   
+
 }
 
 
 // Events
 
-// use DomContentLoaded event fires when page is loaded.
+// use DomContentLoadedevent which fires when page is loaded
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+
+// Event: add a book from form inputs - submit event
+
+// get form first
+document.querySelector("#book-form").addEventListener("submit", 
+(e) => {
+    // prevent page refresh after submit event
+    e.preventDefault();
+    // get each inputs
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const isbn = document.querySelector("#isbn").value;
+
+    // once v get the inouts next instantiate a book object of Book Class.
+    const bookObj = new Book(title, author, isbn);
+    // inputs r passed as arguments to Book class
+
+    // add this inputs to table - invoke addBooks() method - pass bookObj.
+    UI.addBooks(bookObj);
+
+    // invoke clearFields function
+    UI.clearFields();
+})  
+
+// remove a book
+
 
 

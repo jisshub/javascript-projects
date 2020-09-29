0673194854144ec1e74b9f36a6965d089a27518a
jissmon <jisshub>
2020-09-29 09:16:47 +0530

    typewriting effect done

--- typewriter-effects-project/script.js
+++ typewriter-effects-project/script.js
@@ -6,7 +6,6 @@ class TypeWrite {
     this.wordIndex = 0;
     this.txt = '';
     this.isDelete = false;
-    this.type();
   }
   // define type method
   type() {
@@ -16,19 +15,37 @@ class TypeWrite {
     const fullText = this.words[currentIndex];
     if (this.isDelete) {
       // remove character
-      this.txt = fullText.substring(0, -1);
+      this.txt = fullText.substring(0, this.txt.length - 1);
     } else {
       // add character
-      this.txt = fullText.substring(0, 1);
+      this.txt = fullText.substring(0, this.txt.length + 1);
     }
-    const g = document.getElementsByClassName('txt-type');
-    g[0].innerText = this.txt;
-    console.log(g);
+    // insert txt to text Element in a span element
+    this.textElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // set initial type speed
+    let typeSpeed = 300;
+    if (this.isDelete) {
+      typeSpeed /= 300;
+    }
+
+    // if word is complete
+    if (!this.isDelete && fullText === this.txt) {
+      // make a pause at end
+      typeSpeed = this.wait;
+      // set delete to true
+      this.isDelete = true;
+    } else if (this.isDelete && this.txt === '') {
+      this.isDelete = false;
+      // move to next word
+      this.wordIndex++;
+      typeSpeed = 500;
+    }
+
     // call type() after 3sec expires.
-    setTimeout(() => this.type(), 3000);
+    setTimeout(() => this.type(), typeSpeed);
   }
 }
-
 document.addEventListener('DOMContentLoaded', () => {
   // grab the text element
   const textElement = document.querySelector('.txt-type');
@@ -37,5 +54,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // grab the value of data-wait attribute
   const wait = textElement.getAttribute('data-wait');
   // initialize the typeWriter class here
-  new TypeWrite(textElement, words, wait);
+  const n1 = new TypeWrite(textElement, words, wait);
+  n1.type();
 });

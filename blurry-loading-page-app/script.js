9f280d990d6eb640af24e440b7385ffa7e1a2951
jissmon jose <jissmon.jose@naicoits.com>
2021-10-31 21:41:20 +0530

    javascript project

---
+++ blurry-loading-page-app/script.js
@@ -0,0 +1,24 @@
+const loadText = document.querySelector('.loading');
+const bg  = document.querySelector('.bg');
+
+let load = 0;
+
+// run blurring function every 30 millisecond
+
+let interval = setInterval(blurring, 30);
+
+function blurring() {
+    load += 1;
+    if (load > 99) {
+        clearInterval(interval);
+    }
+    loadText.innerHTML = load + '%';
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+// https://stackoverflow.com/a/23202637/104748
